458b0942b20540623b8f6e19bfa3d99fdae185a5
Daniel Yoo <daniel.yoo0523@gmail.com>
2024-12-11 16:45:25 -0500

    added different options

--- ledmenu/Menu.py
+++ ledmenu/Menu.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 from .BoardHandler import BoardHandler
 from .DisplayHandler import DisplayHandler
-from .Options import Options
+from ._Options import Options
 from .Direction import Direction
 from .SnakeWebHandler import SnakeWebHandler
 from .TetrisWebHandler import TetrisWebHandler
@@ -10,8 +10,10 @@ from .ProcessHandler import ProcessHandler
 class Menu:
 
     def __init__(self, options: List[Options], board_handler: BoardHandler, _display_handler: DisplayHandler):
-        self.options = options
-        self.current_index = 0
+        self._options = options
+        self._current_index = 0 #for left/right
+        self._option_setting_index = 0 #for up/down
+        self._option_settings = self._options[0].option_settings
         self._display_handler = _display_handler
         self._board_handler = board_handler
         processHandler = ProcessHandler()
@@ -21,21 +23,30 @@ class Menu:
     def navigate(self, direction):
         # Update the current selection index
         if direction == Direction.RIGHT:
-            self.current_index = (self.current_index - 1) % len(self.options)
+            self._current_index = (self._current_index - 1) % len(self._options)
+            self._option_settings = self._options[0].option_settings
+            self._option_setting_index = 0
         elif direction == Direction.LEFT:
-            self.current_index = (self.current_index + 1) % len(self.options)
+            self._current_index = (self._current_index + 1) % len(self._options)
+            self._option_settings = self._options[0].option_settings
+            self._option_setting_index = 0
+        elif direction == Direction.UP:
+            self._option_setting_index = (self._option_setting_index + 1) % len(self._option_settings)
+        elif direction == Direction.Down:
+            self._option_setting_index = (self._option_setting_index - 1) % len(self._option_settings)
         self.update()
 
     def select_option(self):
         # Return the currently selected option
-        current_option = self.options[self.current_index]
+        current_option = self._options[self._current_index]
+        current_option_setting = current_option.option_settings[self._option_setting_index]
         self._reset_visuals()
         self._display_handler.write_text(current_option.value)
         #self._board_handler.show_graphic(current_option)
         if current_option == Options.TETRIS:
             self._tetrisWebHandler.play_tetris_game()
         elif current_option == Options.SNAKE:
-            self._snakeWebHandler.play_snake_game(True, False)
+            self._snakeWebHandler.play_snake_game(current_option_setting["AI"], current_option_setting["Multiplayer"])
         elif current_option == Options.GIFS:
             print("GIFS")
         print("Selected option")
@@ -45,9 +56,9 @@ class Menu:
         self._display_handler.clear()
 
     def update(self):
-        current_option = self.options[self.current_index]
-        self._board_handler.update(current_option)
-        self._display_handler.write_text(current_option.value)
+        current_option = self._options[self._current_index]
+        self._board_handler.update(current_option) #TODO: handle this
+        self._display_handler.write_text(current_option.option_name + "(" + current_option.option_settings[self._option_setting_index]["Name"] + ")")
 
     def clear(self):
         self._board_handler.turn_off()
--- ledmenu/Options.py
+++ ledmenu/Options.py
@@ -1,6 +1,60 @@
 from enum import Enum
 
 class Options(Enum):
-    TETRIS = "Tetris"
-    SNAKE = "Snake"
-    GIFS = "Gifs"
+    TETRIS = (
+        "Tetris",
+        [
+            {
+                "Name": "1P",
+                "AI": False,
+            },
+            {
+                "Name": "AI",
+                "AI": True,
+            },
+        ],
+    )
+    SNAKE = (
+        "Snake",
+        [
+            {
+                "Name": "1P"
+                "AI": False,
+                "Multiplayer": False,
+            },
+            {
+                "Name": "2P"
+                "AI": False,
+                "Multiplayer": True,
+            },
+            {
+                "Name": "AI"
+                "AI": True,
+                "Multiplayer": False,
+            },
+        ],
+    )
+    GIFS = 
+    (
+        "Gifs",
+        [
+            {
+                "Name": "Test1"
+                "AI": False,
+                "Multiplayer": False,
+            },
+            {
+                "Name": "Test2"
+                "AI": False,
+                "Multiplayer": False,
+            },
+        ],
+    )
+
+    @property
+    def option_name(self):
+        return self.value[0]
+
+    @property
+    def option_settings(self):
+        return self.value[1]
